a266e1ec3f8a622239409097e50b4c7c75135d54
engmakarimi <engineermakarimi@gmail.com>
2023-07-28 19:58:04 +0330

    delete login state

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,20 +3,12 @@ import { Routes } from '@angular/router';
 import { MainLayoutComponent } from './main-layout/main-layout.component';
 import { LoginLayoutComponent } from './pages/login';
 import { provideState } from '@ngrx/store';
-import {
-  AUTH_FEATURE_KEY,
-  authReducer,
-} from './pages/login/state/auth.reducer';
-import { AuthEffects } from './pages/login/state/auth.effects';
+
 import { WEATHER_FEATURE_KEY, WeatherEffects, WeatherFacade, weatherReducer } from './pages/weather/state';
 
 export const routes: Routes = [
   {
     path: 'login',
-    providers: [
-      provideState({ name: AUTH_FEATURE_KEY, reducer: authReducer }),
-      provideEffects(AuthEffects),
-    ],
     component: LoginLayoutComponent,
   },
   { path: '', pathMatch: 'full', redirectTo: '/weather' },
--- src/app/pages/login/state/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './lib/auth.reducer';
-export * from './lib/auth.models';
-export * from './lib/auth.facade';
-export * from './lib/auth.actions';
-export * from './lib/auth.effects';
